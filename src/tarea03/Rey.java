85aa8b990ee938c8f8e9a1d412f804a2c139d5e5
malon <malon@DESKTOP-FC0QCP3>
2017-11-22 21:06:33 +0100

    Creamos el método constructor de la clase Rey.

--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -10,4 +10,21 @@ public class Rey {
     private static Color color;
     private static Posicion posicion;
     
+    /**
+     * Método constructor Rey que recibe el parámetro color. Crea un objeto posición con unos parámetros 
+     * predeterminados dependiendiendo del valor del atributo color. 
+     * @param color Color Puede ser blanco o negro.
+    */
+    public Rey(Color color) {            
+        this.color = color;
+        switch (color) {
+            case BLANCO:
+                posicion = new Posicion(1, 'e');
+                break;
+            case NEGRO:
+                posicion = new Posicion(8, 'e');
+                break;
+        }
+    }//Cierre del constructor
+    
 }
