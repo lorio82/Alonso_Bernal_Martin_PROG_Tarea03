2846647564ac324e13858859f26e53c64a87ffe7
malon <malon@DESKTOP-FC0QCP3>
2017-11-22 20:44:53 +0100

    Creamos la clase Posicion, con sus atributos fila y columna.

---
+++ src/tarea03/Posicion.java
@@ -0,0 +1,16 @@
+
+package tarea03;
+
+
+ /**
+ * Clase Poscion que define objetos posicion caracterizados por un número de fila y un carácter de columna
+ * @author: Martín Alonso Bernal
+ * @version: 11/2017
+ */
+
+public class Posicion {
+    
+    static int fila;
+    static char columna;
+
+}
