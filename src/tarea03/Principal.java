756172a9d3c4caff2cbc7423e0c04eafd35e0a37
malon <malon@DESKTOP-FC0QCP3>
2017-11-22 21:33:15 +0100

    Creamos la clase Principal. Creamos en ella el método main.

---
+++ src/tarea03/Principal.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+import utilidades.Entrada;
+
+ /**
+ * Clase Principal que contiene el método main. 
+ * 
+ * @author: Martín Alonso Bernal
+ * @version: 11/2017
+ */
+public class Principal {
+    
+    public static void main(String[] args) {
+    }
+    
+}
