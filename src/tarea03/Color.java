8112d081603eda5d9adcfb91d659397830c642ff
malon <malon@DESKTOP-FC0QCP3>
2017-11-22 20:35:23 +0100

    Creamos el enumerado Color

---
+++ src/tarea03/Color.java
@@ -0,0 +1,12 @@
+
+package tarea03;
+
+/**
+ * Esta clase crea un enumerado que contiene los dos colores que el usuario puede elegir.
+ * @author: Martín Alonso Bernal
+ * @version: 11/2017
+ */
+
+public enum Color {
+	BLANCO, NEGRO
+}
