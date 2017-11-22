2b72684912816413efef38a0541781396f3571ea
malon <malon@DESKTOP-FC0QCP3>
2017-11-22 21:28:38 +0100

    Creamos el método mueve de la clase Rey. Nota: Incluimos el literal ERROR y la variable errorposicion para verificar que los datos recibidos son correctos, así como para mandar un mensaje de error si fuese necesario

--- src/tarea03/Rey.java
+++ src/tarea03/Rey.java
@@ -10,6 +10,10 @@ public class Rey {
     private static Color color;
     private static Posicion posicion;
     
+    final String ERROR = "Movimiento incorrecto se sale de los límites del tablero";
+    String errorPosicion;
+
+    
     /**
      * Método constructor Rey que recibe el parámetro color. Crea un objeto posición con unos parámetros 
      * predeterminados dependiendiendo del valor del atributo color. 
@@ -53,16 +57,187 @@ public class Rey {
         return posicion;
     }//Cierre del método
     
+        /**
+     * Método mueve recibe una dirección como parámetro y si la nueva posicion es válida,
+ modifica los atributos del objeto posicion según este parámetro,
+ de lo contrario lanza un mensaje de error.
+     * @param direccion Direccion
+    */
+    public void mueve(Direccion direccion) {  
 
+        // El siguiente if determinará la modificación a realizar según el valor del parámetro color del método constructor.
+        if (color == Color.NEGRO) {
+            /**
+             * En el siguiente switch indicamos al programa como proceder según la dirección recibida como parámetro.
+             * En el caso de que el movimiento no esté permitido (por salirse de los límites del tablero)
+             * asignaremos el valor del literall ERROR a la variable errorPosicion
+             */ 
+            switch (direccion) {
+                case NORTE:
+                    if (Posicion.fila == 1) {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(--Posicion.fila);
+                    }
+                    break;
+                case NORESTE:
+                    if (Posicion.fila == 1 || Posicion.columna == 'a') {
+                         errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(--Posicion.fila);
+                    posicion.setColumna(--Posicion.columna);
+                    }
+                    break;
+                case ESTE:
+                    if (Posicion.columna == 'a') {
+                         errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setColumna(--Posicion.columna);
+                    }
+                    break;
+                case SURESTE:
+                    if (Posicion.fila == 8 || Posicion.columna == 'a') {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(++Posicion.fila);
+                    posicion.setColumna(--Posicion.columna);
+                    }
+                    break;
+                case SUR:
+                    if (Posicion.fila == 8) {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(++Posicion.fila);
+                    }
+                    break;
+                case SUROESTE:
+                    if (Posicion.fila == 8 || Posicion.columna == 'h') {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(++Posicion.fila);
+                    posicion.setColumna(++Posicion.columna);
+                    }
+                    break;
+                case OESTE:
+                    if (Posicion.columna == 'h') {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setColumna(++Posicion.columna);
+                    }
+                    break;
+                case NOROESTE:
+                    if (Posicion.fila == 1 || Posicion.columna == 'h') {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(--Posicion.fila);
+                    posicion.setColumna(++Posicion.columna);
+                    }
+                    break;
+            }
+        } 
+        
+        else {
+            switch (direccion) {
+
+                case NORTE:
+                    if (Posicion.fila == 8) {
+                        
+                    }
+                    else {
+                    posicion.setFila(++Posicion.fila);
+                    }
+                    break;
+                case NORESTE:
+                    if (Posicion.fila == 8 || Posicion.columna == 'h') {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(++Posicion.fila);
+                    posicion.setColumna(++Posicion.columna);
+                    }
+                    break;
+                case ESTE:
+                    if (Posicion.columna == 'h') {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setColumna(++Posicion.columna);
+                    }
+                    break;
+                case SURESTE:
+                    if (Posicion.fila == 1 || Posicion.columna == 'h') {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(--Posicion.fila);
+                    posicion.setColumna(++Posicion.columna);
+                    }
+                    break;
+                case SUR:
+                    if (Posicion.fila == 1) {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(--Posicion.fila);
+                    }
+                    break;
+                case SUROESTE:
+                    if (Posicion.fila == 1 || Posicion.columna == 'a') {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(--Posicion.fila);
+                    posicion.setColumna(--Posicion.columna);
+                    }
+                    break;
+                case OESTE:
+                    if (Posicion.columna == 'a') {
+                        errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setColumna(--Posicion.columna);
+                    }
+                    break;
+                case NOROESTE:
+                    if (Posicion.fila == 8 || Posicion.columna == 'a') {
+                         errorPosicion = ERROR;
+                    }
+                    else {
+                    posicion.setFila(++Posicion.fila);
+                    posicion.setColumna(--Posicion.columna);                   
+                    }
+                    break;
+            }
+        }
+    }//Cierre del método
     /**
-     * Método que crea una cadena de caracteres con los valores de color y posicion. 
+     * Método que crea una cadena de caracteres con los valores de color y posicion.
+     * Si la direccion pasada como parámetro no es correcta informará del error
+     * junto a la información anteriormente descrita. 
      * @return String  Devuelve una cadena de caracteres con los valores de color y
-     * posicion.
+     * posicion, e informa del error si es necesario.  
      */
     @Override
     public String toString() {  
-        
+        /**
+         * En el caso de que el contenido de la variable errorPosicion sea igual al del literal ERROR el resultado
+         * de este método devolverá na cadena de caractéres informando del error junto la información relativa al
+         * color y la posicion, si por el contrario la nueva dirección es correcta devolverá una cadena de 
+         * caractéres informando del color y la nueva posición.
+         */
+        if (errorPosicion == ERROR) {
+            return  errorPosicion + "%n" + "Rey " + color +"," + posicion + "%n";
+        }
+        else {
             return "Rey " + color + "," + posicion + "%n";
-      
+        }       
     }//Cierre del método
-}
+}//Cierrre de la clase
