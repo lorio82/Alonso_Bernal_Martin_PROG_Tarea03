657b066ea5a4a06142639ef3b875d77da16f7fc3
malon <malon@DESKTOP-FC0QCP3>
2017-11-22 20:36:52 +0100

    Creamos el enumerado Direccion.

---
+++ src/tarea03/Direccion.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tarea03;
+
+/**
+ * Esta clase crea un enumerado  que contiene las diferentes direcciones que puede elegir el usuario.
+ * @author: Martín Alonso Bernal
+ * @version: 11/2017
+ */
+
+public enum Direccion {
+    NORTE, NORESTE, ESTE, SURESTE, SUR, SUROESTE, OESTE, NOROESTE
+}
